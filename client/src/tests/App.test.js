dab20e5953577f3db076ed45f3c72d14a821d15d
Tim DeCillis <timdecillis@gmail.com>
2023-06-26 11:45:56 -0700

    Add test script to the package.json

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 
-import App from '../components/App';
+import App from '../components/App.jsx';
 
 describe('App', () => {
   it('renders App component', () => {
