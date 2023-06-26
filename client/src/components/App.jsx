a07e1755b44210f1be8056758cb2b8fb6055d545
Tim DeCillis <timdecillis@gmail.com>
2023-06-26 12:56:17 -0700

    Add simple tests to the App.test.js file

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import RatingsAndReviews from './RatingsAndReviews.jsx';
+import RatingsAndReviews from './RatingsAndReviews/RatingsAndReviews.jsx';
 
 const App = () => {
 
