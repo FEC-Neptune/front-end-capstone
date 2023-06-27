f4a616bc35d9f9f84711727a361c93c8044afda4
Tim DeCillis <timdecillis@gmail.com>
2023-06-27 16:29:15 -0700

    Added useState for visibleReviews/setVisibleReviews

--- client/src/components/RatingsAndReviews/ReviewsList.jsx
+++ client/src/components/RatingsAndReviews/ReviewsList.jsx
@@ -8,8 +8,9 @@ import { TOKEN } from '../../../../config.js';
 
 
 const ReviewsList = () => {
-  const [product, setProduct] = useState(40344);
+  const [product, setProduct] = useState(40348);
   const [reviews, setReviews] = useState([]);
+  const [visibleReviews, setVisibleReviews] = useState([]);
 
   useEffect(() => {
     getReviews();
@@ -24,7 +25,9 @@ const ReviewsList = () => {
     return axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/?sort=relevant&product_id=${product}`, options)
       .then((res) => {
         let reviews = res.data.results;
-        setReviews(reviews.slice(0, 2));
+        console.log(reviews);
+        setReviews(reviews);
+        setVisibleReviews(reviews.slice(0, 2));
       })
       .catch((err => {
         throw (err);
@@ -37,7 +40,7 @@ const ReviewsList = () => {
 
   return (
     <>
-      {reviews.map((review) =>
+      {visibleReviews.map((review) =>
         <ReviewTile review={review} key={review.review_id} />
       )}
       <button onClick={addReviews}>MORE REVIEWS</button>
@@ -47,45 +50,4 @@ const ReviewsList = () => {
 };
 
 export default ReviewsList;
-
-
-const dummyData = {
-  'product': '2',
-  'page': 0,
-  'count': 2,
-  'results': [
-    {
-      'review_id': 5,
-      'rating': 3,
-      'summary': 'I\'m enjoying wearing these shades',
-      'recommend': false,
-      'response': null,
-      'body': 'Comfortable and practical.',
-      'date': '2019-04-14T00:00:00.000Z',
-      'reviewer_name': 'shortandsweeet',
-      'helpfulness': 5,
-      'photos': [{
-        'id': 1,
-        'url': 'urlplaceholder/review_5_photo_number_1.jpg'
-      },
-      {
-        'id': 2,
-        'url': 'urlplaceholder/review_5_photo_number_2.jpg'
-      },
-      ]
-    },
-    {
-      'review_id': 3,
-      'rating': 4,
-      'summary': 'I am liking these glasses',
-      'recommend': false,
-      'response': 'Glad you\'re enjoying the product!',
-      'body': 'They are very dark. But that\'s good because I\'m in very sunny spots',
-      'date': '2019-06-23T00:00:00.000Z',
-      'reviewer_name': 'bigbrotherbenjamin',
-      'helpfulness': 5,
-      'photos': [],
-    },
-  ]
-};
 
