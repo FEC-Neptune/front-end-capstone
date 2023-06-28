245feafa936dc6a8b95433362604a5534f0929e0
Patrick Nilan <nilan.patrick@gmail.com>
2023-06-28 10:47:07 -0700

    Updated HTML for Overview subcomponents

--- client/src/components/overview/AddToCart.jsx
+++ client/src/components/overview/AddToCart.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 
 const AddToCart = () => {
   return (
-    <>
-      AddToCart 🫥
-    </>
+    <section id="add-to-cart">
+      <div>
+        <span>Please select a size</span>
+      </div>
+      <div>
+        <form>
+          <select>
+            <option value="A nice style">A nice style</option>
+          </select>
+          <select>
+            <option value="-">-</option>
+          </select>
+          <button type="submit">Add To Cart +</button>
+        </form>
+      </div>
+    </section>
   );
 };
 
