ffccd0b422f035fb7e41b79f2247701e3e7d6cd2
Tim DeCillis <timdecillis@gmail.com>
2023-06-28 15:00:32 -0700

    Debugged the async rendering of the total star ratings

--- client/src/components/RatingsAndReviews/RatingBreakdown.jsx
+++ client/src/components/RatingsAndReviews/RatingBreakdown.jsx
@@ -5,38 +5,46 @@ import { getReviews, getReviewsMeta } from '../../lib/requestHelpers.js';
 
 const RatingBreakdown = ({ reviews }) => {
 
-  const [metaData, setMetaData] = useState('foo');
+  const [oneStar, setOneStar] = useState('');
+  const [twoStar, setTwoStar] = useState('');
+  const [threeStar, setThreeStar] = useState('');
+  const [fourStar, setFourStar] = useState('');
+  const [fiveStar, setFiveStar] = useState('');
 
   useEffect(() => {
-    getReviewsMeta(40347)
-      .then((data) => {
-        setMetaData(data.data);
+    getReviewsMeta(40346)
+      .then(({ data }) => {
+        let ratings = data.ratings;
+        console.log(ratings);
+        let totalReviews = calculateTotalReviews(ratings);
+        setOneStar(calculateRatingsPercentage(ratings['1'], totalReviews));
+        setTwoStar(calculateRatingsPercentage(ratings['2'], totalReviews));
+        setThreeStar(calculateRatingsPercentage(ratings['3'], totalReviews));
+        setFourStar(calculateRatingsPercentage(ratings['4'], totalReviews));
+        setFiveStar(calculateRatingsPercentage(ratings['5'], totalReviews));
       });
   }, []);
 
-  const calculateTotalReviews = (data) => {
+  const calculateTotalReviews = (ratingsObject) => {
     var sum = 0;
-    for (var key in data.ratings) {
-      sum += data.ratings[key];
+    for (var key in ratingsObject) {
+      sum += parseInt(ratingsObject[key]);
     }
     return sum;
   };
 
-  const calculateRatingsPercentage = (starRating) => {
-    // console.log(starRating);
-    let totalReviews = calculateTotalReviews(metaData);
+  const calculateRatingsPercentage = (starRating, totalReviews) => {
     return Math.round(starRating * 100 / totalReviews);
   };
-
 
   return (
     <>
       <div>Average Rating: {averageRating(40347)}</div>
-      <div>5 Stars: {calculateRatingsPercentage(13)}%</div>
-      {/* <div>4 Stars: {(ratingsCounter[4] * 100) / (totalReviews.length)}%</div>
-      <div>3 Stars: {(ratingsCounter[3] * 100) / (totalReviews.length)}%</div>
-      <div>2 Stars: {(ratingsCounter[2] * 100) / (totalReviews.length)}%</div>
-      <div>1 Stars: {(ratingsCounter[1] * 100) / (totalReviews.length)}%</div> */}
+      <div>5 Stars: {fiveStar}%</div>
+      <div>4 Stars: {fourStar}%</div>
+      <div>3 Stars: {threeStar}%</div>
+      <div>2 Stars: {twoStar}%</div>
+      <div>1 Stars: {oneStar}%</div>
     </>
   );
 };
