87bb526d5b205e71e81e6302981931f4b87ac093
Patrick Nilan <nilan.patrick@gmail.com>
2023-06-28 11:06:59 -0700

    added helper function files in lib

---
+++ client/src/lib/ratingsAndReviewsHelpers.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  averageRating: (ratingsObj) => {
+    let totalRatings = 0;
+    let totalScore = 0;
+
+    Object.keys(ratingsObj).forEach((score) => {
+      totalRatings += ratingsObj[score];
+      totalScore += ratingsObj[score] * score;
+    });
+
+    let avgRating = totalScore / totalRatings;
+
+    avgRating = (Math.round(avgRating * 4) / 4).toFixed(2);
+    return avgRating;
+  }
+
+};
