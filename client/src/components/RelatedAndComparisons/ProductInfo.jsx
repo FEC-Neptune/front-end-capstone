ba655a12b995a406655daee09c66c3b79f737643
Aaron Brandenberger <a.branden.mi@gmail.com>
2023-06-28 10:33:23 -0600

    Add axios call for future get requests

--- client/src/components/RelatedAndComparisons/ProductInfo.jsx
+++ client/src/components/RelatedAndComparisons/ProductInfo.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { useState } from 'react';
 
-const ProductInfo = () => (
-  <div>
-    <div>Category</div>
-    <div>Name</div>
-    <div>Price</div>
-    <div>*****</div>
-  </div>
-);
+
+const ProductInfo = () => {
+
+  const [category, setCategory] = useState('N/A');
+  const [name, setName] = useState('N/A');
+  const [price, setPrice] = useState('N/A');
+  const [rating, setRating] = useState('N/A');
+
+  return (
+    <div>
+      <div>Category</div>
+      <div>Name</div>
+      <div>Price</div>
+      <div>*****</div>
+    </div>
+  );
+};
 
 export default ProductInfo;
--- client/src/components/RelatedAndComparisons/RelatedAndComparisons.jsx
+++ client/src/components/RelatedAndComparisons/RelatedAndComparisons.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { TOKEN } from '../../../../config.js';
 
-const RelatedAndComparisons = () => (
-  <div>
-    <div>Related Component</div>
-    <div>Outfit Component</div>
-  </div>
-);
+const RelatedAndComparisons = () => {
+
+
+  //API Call that takes in optional id (INT) and category ('styles' or 'related')
+  //returns full product list array if no params
+  //returns single product object if only id
+  //returns styles or related products array if id and category
+  const fetchProducts = function (id, category) {
+    let options = {
+      headers: {
+        'Authorization': TOKEN
+      }
+    };
+    id ? id = id + '/' : id = '';
+    category ? category = category : category = '';
+    return axios
+      .get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${id}${category}`, options)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div>
+      <div>Related Component</div>
+      <div>Outfit Component</div>
+    </div>
+  );
+
+};
 
 export default RelatedAndComparisons;
