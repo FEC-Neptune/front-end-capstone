730065835087039ade353072f340fb20a4b4e0ca
Aaron Brandenberger <a.branden.mi@gmail.com>
2023-06-28 15:05:20 -0600

    Merge conflict resolution

--- client/src/lib/requestHelpers.js
+++ client/src/lib/requestHelpers.js
@@ -1,31 +1,28 @@
 import { TOKEN } from '../../../config.js';
 import axios from 'axios';
 
+module.exports = {
+
 //API Call that takes in optional id (INT) and category ('styles' or 'related')
 //returns full product list array if no params
 //returns single product object if only id
 //returns styles or related products array if id and category
-const fetchProducts = function (id, category) {
-  let options = {
-    headers: {
-      'Authorization': TOKEN
-    }
+  fetchProducts: function (id, category) {
+    let options = {
+      headers: {
+        'Authorization': TOKEN
+      }
+    };
+
+    id = id ? id + '/' : '';
+    category = category ? category : '';
+    return axios
+      .get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${id}${category}`, options)
+      .then((res) => (res.data))
+      .catch((err => {
+        throw (err);
+      }));
   };
-
-  id = id ? id + '/' : '';
-  category = category ? category : '';
-  return axios
-    .get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${id}${category}`, options)
-    .then((res) => (res.data))
-    .catch((err => {
-      throw (err);
-    }));
-};
-
-// get product
-// get product styles
-// get product reviews meta
-module.exports = {
 
   getReviews: (id) => {
     var url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/?sort=relevant&product_id=' + id;
