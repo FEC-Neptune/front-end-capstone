4634818dda677805c620b3a93d559fd03cb3cbeb
Tim DeCillis <timdecillis@gmail.com>
2023-06-28 11:53:57 -0700

    Add getReviews and getReviewsMeta helper functions

--- client/src/lib/requestHelpers.js
+++ client/src/lib/requestHelpers.js
@@ -3,3 +3,41 @@ import { TOKEN } from '../../../config.js';
 // get product
 // get product styles
 // get product reviews meta
+module.exports = {
+
+  getReviews: (id) => {
+    var url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/?sort=relevant&product_id=' + id;
+
+    axios({
+      method: 'get',
+      url: url,
+      headers: {
+        'Authorization': TOKEN
+      }
+    })
+      .then((res) => {
+        let reviews = res.data.results;
+        console.log(reviews);
+      })
+      .catch((err => {
+        throw (err);
+      }));
+  },
+  getReviewsMeta: (id) => {
+    var url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/meta/?product_id=' + id;
+
+    axios({
+      method: 'get',
+      url: url,
+      headers: {
+        'Authorization': TOKEN
+      }
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err => {
+        throw (err);
+      }));
+  }
+};
