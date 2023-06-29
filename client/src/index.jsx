8ab5e05bbba43ff9e6871903402bc59abb3e48b8
Aaron Brandenberger <a.branden.mi@gmail.com>
2023-06-29 15:38:33 -0600

    Import styles

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './assets/styles.sass';
+import './assets/related-and-comparisons.sass';
 import App from './components/App.jsx';
 
 const domNode = document.getElementById('root');
