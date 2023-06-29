2c559412c07804b6b98a39e42a02f4922e69fe8b
Aaron Brandenberger <a.branden.mi@gmail.com>
2023-06-29 17:26:54 -0600

    styling changes

--- client/src/components/RelatedAndComparisons/ProductInfo.jsx
+++ client/src/components/RelatedAndComparisons/ProductInfo.jsx
@@ -5,9 +5,9 @@ import { useState } from 'react';
 const ProductInfo = ( { product } ) => {
 
   return (
-    <div>
+    <div className='info-container'>
+      <div className='info'><div className='info-name-border'><div className='info-name-text'>{product.name}</div></div></div>
       <div className='info'>{product.category}</div>
-      <div className='info'>{product.name}</div>
       <div className='info'>{product.default_price}</div>
       <div className='info'>*****</div>
     </div>
