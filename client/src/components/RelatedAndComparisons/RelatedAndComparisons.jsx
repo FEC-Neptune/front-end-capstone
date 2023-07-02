ec2d376a5be53f9f6c132ed5a250e978e8a77a8a
Aaron Brandenberger <a.branden.mi@gmail.com>
2023-07-01 18:50:55 -0600

    Build carousel functionality for related and outfits carousels

--- client/src/components/RelatedAndComparisons/RelatedAndComparisons.jsx
+++ client/src/components/RelatedAndComparisons/RelatedAndComparisons.jsx
@@ -8,6 +8,10 @@ import Outfit from './Outfit.jsx';
 const RelatedAndComparisons = () => {
   const [prodArr, setProdArr] = useState([]);
   const [currentThumbnail, setCurrentThumbnail] = useState('');
+  const [relatedIndex, setRelatedIndex] = useState(0);
+  const [outfitIndex, setOutfitIndex] = useState(0);
+  const [relatedPosition, setRelatedPosition] = useState();
+  const [outfitPosition, setOutfitPosition] = useState();
 
   useEffect(() => {
     fetchProducts()
@@ -18,13 +22,61 @@ const RelatedAndComparisons = () => {
   }, []);
 
 
+  const scrollToCard = (cardIndex, buttonId) => {
+    buttonId.includes('related') ? setRelatedPosition(cardIndex * 238) : setOutfitPosition(cardIndex * 238);
+  };
+
+  const handleLeftClick = () => {
+    const buttonId = event.target.id;
+    const arrLength = prodArr.length - 1;
+
+    if (buttonId.includes('related')) {
+      if (-arrLength !== relatedIndex) {
+        console.log(relatedIndex);
+        setRelatedIndex((prevIndex) => {
+          const newIndex = prevIndex - 1;
+          scrollToCard(newIndex, buttonId);
+          return newIndex;
+        });
+      }
+    } else {
+      if (-arrLength !== outfitIndex) {
+        setOutfitIndex((prevIndex) => {
+          const newIndex = prevIndex - 1;
+          scrollToCard(newIndex, buttonId);
+          return newIndex;
+        });
+      }
+    }
+  };
+
+  const handleRightClick = () => {
+    const buttonId = event.target.id;
+    if (buttonId.includes('related')) {
+      if (relatedIndex !== 0) {
+        setRelatedIndex((prevIndex) => {
+          const newIndex = prevIndex + 1;
+          scrollToCard(newIndex, buttonId);
+          return newIndex;
+        });
+      }
+    } else {
+      if (outfitIndex !== 0) {
+        setOutfitIndex((prevIndex) => {
+          const newIndex = prevIndex + 1;
+          scrollToCard(newIndex, buttonId);
+          return newIndex;
+        });
+      }
+    }
+  };
 
   return (
     <div id='widget'>
       <div className='carousel-title-container'><div className='carousel-title'>Related Items</div></div>
-      <Related products={prodArr} fetchProducts={fetchProducts} />
+      <Related products={prodArr} fetchProducts={fetchProducts} handleLeftClick={handleLeftClick} handleRightClick={handleRightClick} scrollPosition={relatedPosition}/>
       <div className='carousel-title-container'><div className='carousel-title'>Your Outfit</div></div>
-      <Outfit outfitItems={prodArr} />
+      <Outfit outfitItems={prodArr} handleLeftClick={handleLeftClick} handleRightClick={handleRightClick} scrollPosition={outfitPosition}/>
     </div>
   );
 
