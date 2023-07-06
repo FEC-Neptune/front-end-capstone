a81d4b5fd415ee7973c446810a03565576d70932
Tim DeCillis <timdecillis@gmail.com>
2023-07-05 20:10:28 -0700

    Added simple test for Ratings and Reviews, added jest.config.js file, added jest.config.js to gitignore

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -6,15 +6,5 @@ import App from '../components/App.jsx';
 describe('App', () => {
   it('renders App component', () => {
     render(<App />);
-    // screen.debug();
   });
-  it('renders the header text', () => {
-    render(<App />);
-    screen.getByText(/Hello/);
-  });
-  it('renders the ratings text', () => {
-    render(<App />);
-    screen.getByText('Ratings and Reviews');
-  });
-
 });
