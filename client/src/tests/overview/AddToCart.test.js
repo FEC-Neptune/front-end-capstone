d7eec6e99d9997b6418ef98361e32d5632e9b19d
Patrick Nilan <nilan.patrick@gmail.com>
2023-07-05 23:54:07 -0700

    53% test coverage for Overview component(s)

--- client/src/tests/overview/AddToCart.test.js
+++ client/src/tests/overview/AddToCart.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react';
+import {render, screen, fireEvent} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { productId, currentProduct, style, reviewsData, productStyles } from './overviewData.js';
 
@@ -7,9 +7,39 @@ import AddToCart from '../../components/overview/AddToCart.jsx';
 
 describe('Add to Cart', function() {
 
-  it('should render the AddToCart component', function() {
+  beforeEach(() => {
     render(<AddToCart style={style} />);
+  });
+
+  it('should render the AddToCart component', function() {
     var element = document.querySelector('#add-to-cart');
     expect(element).toBeInTheDocument();
   });
+
+  it('should render the qty select input', function() {
+    expect(document.querySelector('#qty-select')).toBeTruthy();
+  });
+
+  it('should render the size select input', function() {
+    expect(document.querySelector('#size-select')).toBeTruthy();
+  });
+
+  it('should render the select options for a given qty', function() {
+    expect(document.getElementsByTagName('option').length).toBe(13);
+  });
+
+  it('should render the add to cart button', function() {
+    expect(document.querySelector('#add-to-cart-button')).toBeTruthy();
+  });
+
+  it('should render the add to cart form', function() {
+    expect(document.getElementsByTagName('form')).toBeTruthy();
+  });
+
+  it('should populat the form properly', function() {
+    expect(screen.getByText('7.5')).toBeInTheDocument();
+    expect(screen.getByText('Add To Cart')).toBeInTheDocument();
+    expect(screen.getByText('Select a size')).toBeInTheDocument();
+  });
+
 });
--- client/src/tests/overview/ImageGallery.test.js
+++ client/src/tests/overview/ImageGallery.test.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {render, screen} from '@testing-library/react';
+import React, {useState} from 'react';
+import {render, screen, fireEvent} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { productId, currentProduct, style, reviewsData, productStyles } from './overviewData.js';
 
@@ -11,10 +11,80 @@ import ImageGallery from '../../components/overview/ImageGallery.jsx';
 
 describe('ImageGallery', function() {
 
-  const activeImageIndex = 0;
-  const expandedView = false;
+  const Wrapper = () => {
+    const [activeImageIndex, setActiveImageIndex] = useState(0);
+    const [expandedView, setExpandedView] = useState(false);
+    return <ImageGallery product={productId} style={style} activeImageIndex={activeImageIndex} expandedView={expandedView} setActiveImageIndex={setActiveImageIndex} setExpandedView={setExpandedView} />;
+  };
+
+  beforeEach(() => {
+    render(<Wrapper />);
+  });
 
   it('should render the ImageGallery component', function() {
-    render(<ImageGallery product={productId} style={style} activeImageIndex={activeImageIndex} expandedView={expandedView} />);
+    expect(document.querySelector('.image-gallery')).toBeTruthy();
   });
+
+  it('should render the ImageGallery component', function() {
+    expect(document.getElementsByClassName('image-gallery')).toBeTruthy();
+  });
+
+  it('should render the thumbnail container', function() {
+    expect(document.getElementById('thumbnail-container')).toBeTruthy();
+  });
+
+  it('should render thumbnails', function() {
+    expect(document.getElementsByClassName('ig-thumbnail')).toBeTruthy();
+  });
+
+  it('should render svg icons', function() {
+    expect(document.getElementsByClassName('ig-nav')).toBeTruthy();
+  });
+
+  it('should render the image navigation container', function() {
+    expect(document.getElementById('image-nav-container')).toBeTruthy();
+  });
+
+  it('should render 7 thumbnails', function() {
+    expect(document.getElementsByClassName('ig-thumbnail').length).toBe(7);
+  });
+
+  it('should toggle the expanded view when the clicked', function() {
+    fireEvent.click(document.getElementById('image-gallery-default'));
+    expect(document.getElementById('image-gallery-default')).toBeFalsy();
+  });
+
+  it('should toggle the expanded view when the clicked and then toggle zoom mode when clicked again', function() {
+    fireEvent.click(document.getElementById('image-gallery-default'));
+    expect(document.getElementById('image-gallery-expanded')).toBeTruthy();
+    fireEvent.click(document.getElementById('image-gallery-expanded'));
+    expect(document.getElementsByClassName('zoom-image')).toBeTruthy();
+  });
+
+
+  it('after toggling expanded view, elements should render', function() {
+    fireEvent.click(document.getElementById('image-gallery-default'));
+    expect(document.getElementById('image-gallery-default')).toBeFalsy();
+    expect(document.getElementById('expanded-image-icon-container')).toBeTruthy();
+    expect(document.getElementById('expanded-nav-container')).toBeTruthy();
+    expect(document.getElementsByClassName('ig-thumbnail').length).toBeFalsy();
+    expect(document.getElementsByClassName('svg-inline--fa fa-square fa-fw fa-xs ig-icons')).toBeTruthy();
+    expect(document.getElementById('expanded-nav-minimize-container')).toBeTruthy();
+    expect(document.getElementById('expanded-nav-chevron-container')).toBeTruthy();
+  });
+
+  it('after toggling zoom mode, elements should render', function() {
+    fireEvent.click(document.getElementById('image-gallery-default'));
+    fireEvent.click(document.getElementById('image-gallery-expanded'));
+    expect(document.getElementById('image-gallery-default')).toBeFalsy();
+    expect(document.getElementById('expanded-image-icon-container')).toBeFalsy();
+    expect(document.getElementById('expanded-nav-container')).toBeFalsy();
+    expect(document.getElementsByClassName('ig-thumbnail').length).toBeFalsy();
+    expect(document.getElementsByClassName('svg-inline--fa fa-square fa-fw fa-xs ig-icons').length).toBeFalsy();
+    expect(document.getElementById('expanded-nav-minimize-container')).toBeFalsy();
+    expect(document.getElementById('expanded-nav-chevron-container')).toBeFalsy();
+    expect(document.getElementsByClassName('image-zoom-container')).toBeTruthy();
+    expect(document.getElementsByTagName('img')).toBeTruthy();
+  });
+
 });
